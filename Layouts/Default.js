5ef7ca41a9b37f581e9314e51ba86f166f1ae69b
dimer191996 <db191996@gmail.com>
2022-08-12 07:07:36 +0200

    new update

--- Layouts/Default.js
+++ Layouts/Default.js
@@ -6,22 +6,48 @@ import NavBar from "../components/NavBar";
 
 export default function Default({ children }) {
   const [visible, setVisible] = useState(false);
+   const [showArticle, setShowArticle] = useState(false);
   useEffect(() => {
     if (window.location.search.includes("success=true")) {
       setVisible(false);
     }
   }, []);
+
+    const timeleft = 20;
+    const downloadTimer = setInterval(function () {
+      if (timeleft <= 0) {
+        setShowArticle(true);
+        clearInterval(downloadTimer);
+      }
+      document.getElementById("progressBar")?.value = 20 - timeleft;
+      timeleft -= 1;
+    }, 2000);
+    
   return (
     <>
       <div className="">
-        <NavBar />
-        <main className="container m-auto"> {children}</main>
-        {/* <Footer /> */}
+        {!showArticle && (
+          <>
+         
+            <div className=" flex relative justify-center items-center">
+              <div className=" absolute text-white">Click to continue or wait 20s</div>
+              <progress
+                className="w-full"
+                value="0"
+                max="20"
+                id="progressBar"
+              ></progress>
+            </div>
+          </>
+        )}
+       
+       {showArticle && <> <NavBar /> <main className="container m-auto"> {children}</main>
+        <Footer />
         <FloatingButton
           hidebutton={() => setVisible(false)}
           showbutton={() => setVisible(true)}
           hideHeader={visible}
-        />
+        /></>}
       </div>
     </>
   );
