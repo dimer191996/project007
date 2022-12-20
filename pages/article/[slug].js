e40cfd6bb32283676e36cf69d37947783d593e12
Dimer Bwimba <78191130+dimer191996@users.noreply.github.com>
2022-12-20 11:36:38 +0000

    zergnet add

--- pages/article/[slug].js
+++ pages/article/[slug].js
@@ -182,7 +182,7 @@ const Post = ({ article }) => {
               ) : (
                 <div>
                   <div className="separator"></div>
-                    <div id="zergnet-widget-91287"></div>
+                    <div id="zergnet-widget-90952"></div>
                   <div className="separator"></div>
                 </div>
               )}
