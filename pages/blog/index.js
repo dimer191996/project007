34c7d9a318f274403ea03079f099f4ac5c546fa0
Dimer Bwimba <78191130+dimer191996@users.noreply.github.com>
2023-01-07 05:59:50 +0000

    change rss feed comfig

--- pages/blog/index.js
+++ pages/blog/index.js
@@ -1,49 +1,49 @@
-// import axios from "axios";
-// import { Feed } from 'feed';
-// import  fs from 'fs';
+import axios from "axios";
+import { Feed } from 'feed';
+import  fs from 'fs';
 
 
-// export async function getStaticProps() {
-//     const site_url = 'https://www.hotseatmag.com';
-//     const allPosts = await axios(`${site_url}/api/articles`).then(({ data }) => {
-//       return data
-//     });
+export async function getStaticProps() {
+    const site_url = 'https://www.hotseatmag.com';
+    const allPosts = await axios(`${site_url}/api/articles`).then(({ data }) => {
+      return data
+    });
 
-//     const feed = new Feed({
-//       title: 'WELCOME TO HOT SEAT MAGAZINE | RSS Feed',
-//       description: 'WELCOME TO HOT SEAT MAGAZINE ARTICLES UPDATES!',
-//       site_url: site_url,
-//       feed_url: `${site_url}/rss.xml`,
-//       image: `${site_url}/logo.png`,
-//       favicon: `${site_url}/favicon.png`,
-//       pubDate: new Date(),
-//       copyright: `All rights reserved ${new Date().getFullYear()}, HotSeatMag`,
-//       feedLinks: {
-//         rss2: `${site_url}/rss.xml`,
-//       },
-//     });
+    const feed = new Feed({
+      title: 'WELCOME TO HOT SEAT MAGAZINE | RSS Feed',
+      description: 'WELCOME TO HOT SEAT MAGAZINE ARTICLES UPDATES!',
+      site_url: site_url,
+      feed_url: `${site_url}/rss.xml`,
+      image: `${site_url}/logo.png`,
+      favicon: `${site_url}/favicon.png`,
+      pubDate: new Date(),
+      copyright: `All rights reserved ${new Date().getFullYear()}, HotSeatMag`,
+      feedLinks: {
+        rss2: `${site_url}/rss.xml`,
+      },
+    });
 
-//     allPosts.forEach((post) => {
-//       feed.addItem({
-//         title: post.title,
-//         id: `${site_url}/article/${post.slug}`,
-//         link: `${site_url}/article/${post.slug}`,
-//         description: post.description,
-//         date: new Date(post.createdAt),
-//       });
-//     });
+    allPosts.forEach((post) => {
+      feed.addItem({
+        title: post.title,
+        id: `${site_url}/article/${post.slug}`,
+        link: `${site_url}/article/${post.slug}`,
+        description: post.description,
+        date: new Date(post.createdAt),
+      });
+    });
 
-//     fs.writeFileSync('./public/rss.xml', feed.rss2());
+    fs.writeFileSync('./public/rss.xml', feed.rss2());
 
-//     return {
-//       props:{ }
-//     }
+    return {
+      props:{ }
+    }
 
-// }
+}
 
 export default function RssFeed({ rss }) {
   return (
-    <div>hello world</div>
+    <div></div>
   )
   // Use the `rss` data as needed in your application
 }
