a809df9e503d09c975c5050c440afc9cecb18168
db191996 <db191996@gmail.com>
2023-01-25 10:38:40 +0100

    updat rss to atom

--- pages/blog/index.js
+++ pages/blog/index.js
@@ -4,36 +4,38 @@ import  fs from 'fs';
 
 
 export async function getStaticProps() {
-    const site_url = 'https://www.hotseatmag.com';
-    const allPosts = await axios(`${site_url}/api/articles`).then(({ data }) => {
+    const site_url = 'https://www.hotseatmag.com/';
+    const latest = await axios(`${site_url}api/articles`).then(({ data }) => {
       return data
     });
 
     const feed = new Feed({
-      title: 'WELCOME TO HOT SEAT MAGAZINE | RSS Feed',
-      description: 'WELCOME TO HOT SEAT MAGAZINE ARTICLES UPDATES!',
+      title: 'Latest & Breaking News on Hot Seat Magazine',
+      description: 'Discover the latest breaking news feed with Hot Seat Magazine. Find out what the latest news is and read about the latest news happening today.',
       site_url: site_url,
-      feed_url: `${site_url}/rss.xml`,
-      image: `${site_url}/logo.png`,
-      favicon: `${site_url}/favicon.png`,
+      feed_url: `${site_url}rss.xml`,
+      image: `${site_url}logo.png`,
+      favicon: `${site_url}favicon.png`,
       pubDate: new Date(),
       copyright: `All rights reserved ${new Date().getFullYear()}, HotSeatMag`,
       feedLinks: {
-        rss2: `${site_url}/rss.xml`,
+        // rss2: `${site_url}rss.xml`,
+        atom1: `${site_url}/latest.xml`,
       },
     });
-
-    allPosts.forEach((post) => {
+   if(!latest){alert("no data")}
+    latest.forEach((post) => {
       feed.addItem({
         title: post.title,
-        id: `${site_url}/article/${post.slug}`,
-        link: `${site_url}/article/${post.slug}`,
+        id: `${site_url}article/${post.slug}`,
+        link: `${site_url}article/${post.slug}`,
         description: post.description,
+        image:post.image,
         date: new Date(post.createdAt),
       });
     });
 
-    fs.writeFileSync('./public/rss.xml', feed.rss2());
+    fs.writeFileSync('./public/latest.xml', feed.atom1());
 
     return {
       props:{ }
