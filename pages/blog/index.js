79c34c3824ffde3c4fba91fb9ca5095638237b4b
db191996 <db191996@gmail.com>
2023-04-01 11:19:35 +0100

    blog/rss -rm

--- pages/blog/index.js
+++ pages/blog/index.js
@@ -1,49 +1,49 @@
-import axios from "axios";
-import { Feed } from 'feed';
-import  fs from 'fs';
+// import axios from "axios";
+// import { Feed } from 'feed';
+// import  fs from 'fs';
 
 
-export async function getStaticProps() {
-    const site_url = 'https://www.hotseatmag.com/';
-    const latest = await axios(`${site_url}api/articles/latest`).then(({ data }) => {
-      return data
-    }).catch((error)=>{
-      consol.log(error)
-    });
+// export async function getStaticProps() {
+//     const site_url = 'https://www.hotseatmag.com/';
+//     const latest = await axios(`${site_url}api/articles/latest`).then(({ data }) => {
+//       return data
+//     }).catch((error)=>{
+//       consol.log(error)
+//     });
 
-    const feed = new Feed({
-      title: 'Latest & Breaking News on Hot Seat Magazine',
-      description: 'Discover the latest breaking news feed with Hot Seat Magazine. Find out what the latest news is and read about the latest news happening today.',
-      site_url: site_url,
-      feed_url: `${site_url}rss.xml`,
-      image: `${site_url}logo.png`,
-      favicon: `${site_url}favicon.png`,
-      pubDate: new Date(),
-      copyright: `All rights reserved ${new Date().getFullYear()}, HotSeatMag`,
-      feedLinks: {
-        // rss2: `${site_url}rss.xml`,
-        atom1: `${site_url}/latest.xml`,
-      },
-    });
+//     const feed = new Feed({
+//       title: 'Latest & Breaking News on Hot Seat Magazine',
+//       description: 'Discover the latest breaking news feed with Hot Seat Magazine. Find out what the latest news is and read about the latest news happening today.',
+//       site_url: site_url,
+//       feed_url: `${site_url}rss.xml`,
+//       image: `${site_url}logo.png`,
+//       favicon: `${site_url}favicon.png`,
+//       pubDate: new Date(),
+//       copyright: `All rights reserved ${new Date().getFullYear()}, HotSeatMag`,
+//       feedLinks: {
+//         // rss2: `${site_url}rss.xml`,
+//         atom1: `${site_url}/latest.xml`,
+//       },
+//     });
  
-    latest?.forEach((post) => {
-      feed.addItem({
-        title: post.title,
-        id: `${site_url}article/${post.slug}`,
-        link: `${site_url}article/${post.slug}`,
-        description: post.description,
-        image:post.image,
-        date: new Date(post.createdAt),
-      });
-    });
+//     latest?.forEach((post) => {
+//       feed.addItem({
+//         title: post.title,
+//         id: `${site_url}article/${post.slug}`,
+//         link: `${site_url}article/${post.slug}`,
+//         description: post.description,
+//         image:post.image,
+//         date: new Date(post.createdAt),
+//       });
+//     });
 
-    fs.writeFileSync('./public/latest.xml', feed.atom1());
+//     fs.writeFileSync('./public/latest.xml', feed.atom1());
     
-    return {
-      props:{ }
-    }
+//     return {
+//       props:{ }
+//     }
 
-}
+// }
 
 export default function RssFeed({ rss }) {
   return (
